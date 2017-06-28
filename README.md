def6bc9d72500bf252c9bf36ecd3790d6d0d621f
Kong <kongPro@Kongs-MacBook-Pro-6.local>
2017-06-28 15:04:18 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 2017ClusterDecayModel
+
+A simple model to test local charge conservation coupled with v2
+
+To compile it, do:
+
+xxxx
---
+++ src/ClusterProducer.cpp
@@ -0,0 +1,19 @@
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <sstream>
+
+#include "TROOT.h"
+#include "TH1D.h"
+#include "TCanvas.h"
+
+using namespace std;
+
+int main(){
+
+	cout << "hello world" << endl;
+
+}
